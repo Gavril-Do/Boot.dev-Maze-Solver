64642be909eb06cf95eb7659db9e889357c2852c
Gavril-Do <17620963+Gavril-Do@users.noreply.github.com>
2025-01-23 13:32:23 -0800

    L3.1: Create a 'window' Class Using Tkinter
    Changes to be committed:
    	new file:   .gitignore
    	new file:   __pycache__/window.cpython-310.pyc
    	new file:   main.py
    	new file:   window.py

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter import Tk, BOTH, Canvas
+from window import Window
+
+def main():
+	win = Window(400, 300)
+	win.wait_for_close()
+
+
+
+if __name__ == "__main__":
+	main()
---
+++ window.py
@@ -0,0 +1,22 @@
+from tkinter import Tk, BOTH, Canvas
+
+class Window(Tk):
+	def __init__(self, width=800, height=600):
+		self.__root = Tk()
+		self.title = "Title"
+		self.canvas = Canvas(self, width=width, height=height)
+		self.canvas.pack(fill=BOTH, expand=True)
+		self.running = False
+		self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+	def redraw(self):
+		self.__root.update_idletasks()
+		self.__root.update()
+	
+	def wait_for_close(self):
+		self.running = True
+		while self.running == True:
+			self.redraw()
+		
+	def close(self):
+		self.running = False
